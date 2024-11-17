5cd9de0506a4c68e3a67cd2463518108393ec97d
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-16 23:56:13 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('Olá, Mundo!')
+
+var userName = 'Gabriel Freitas'
+
+document.getElementById('userName').innerHTML = userName
+
+/*
+var nome = 'Yoda'
+var idade = 100
+var jedi = true
+
+console.log(nome)
+console.log(idade)
+console.log(jedi)
+*/
