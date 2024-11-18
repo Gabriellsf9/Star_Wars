53fac8101aaacbe3155ae024a889276051cd8527
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-18 14:43:19 -0300

    integrando Vue Js no projeto e criando botão de like

--- js/app.js
+++ js/app.js
@@ -1,15 +1,65 @@
-console.log('Olá, Mundo!')
 
-var userName = 'Gabriel Freitas'
 
-document.getElementById('userName').innerHTML = userName
+const LIST = [
+    {
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'Vader',
+        avatar: 'images/vader.png'
+    },
+    
+    {
+        id: 3,
+        nome: 'R2D2',
+        avatar: 'images/r2d2.png'
+    },
+    
+    {
+        id: 4,
+        nome: 'Luke',
+        avatar: 'images/luke.png'
+    },
 
-/*
-var nome = 'Yoda'
-var idade = 100
-var jedi = true
+    {
+        id: 5,
+        nome: 'Leia',
+        avatar: 'images/leia.png'
+    },
+    
+    {
+        id: 6,
+        nome: 'Hansolo',
+        avatar: 'images/hansolo.png'
+    },
+    
+    {
+        id: 7,
+        nome: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
+    },
+    
+    {
+        id: 8,
+        nome: 'C3PO',
+        avatar: 'images/c3po.png'
+    }
+]
 
-console.log(nome)
-console.log(idade)
-console.log(jedi)
-*/
+
+const App = new Vue({
+    el: '#app',
+    data: {
+        title: 'Star Wars Lego',
+        userName: 'Gabriel Freitas',
+        characters: LIST
+    },
+    methods: {
+        like(userName) {
+            alert(`O Personagem ${userName} recebeu um like!`)
+        }
+    }
+})
---
+++ js/basic.js
@@ -0,0 +1,26 @@
+console.log('Olá, Mundo!')
+
+var userName = 'Gabriel Freitas'
+
+document.getElementById('userName').innerHTML = userName
+
+//Variaveis //
+
+/*
+var nome = 'Yoda'
+var idade = 100
+var jedi = true
+
+console.log(nome)
+console.log(idade)
+console.log(jedi)
+*/
+
+// Operadores Matematicos //
+
+// var n1 = 5
+//var n2 = 5
+//var total = n1 + n2
+//console.log(total)
+
+// Operadores Comparação  //
